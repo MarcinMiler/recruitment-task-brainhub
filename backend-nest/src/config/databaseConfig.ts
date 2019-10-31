27109d4d8be8a42a116f2732a9dc4ddcc204f738
Marcin Miler <marcin4951@gmail.com>
2019-10-31 21:15:49 +0100

    [#2] setup database and typeorm

---
+++ backend-nest/src/config/databaseConfig.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
+const env = (val: string) => process.env[val]
+
+export const databaseOptions: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: env('DB_HOST'),
+    port: Number(env('PG_PORT')),
+    username: env('POSTGRES_USER'),
+    password: env('POSTGRES_PASSWORD'),
+    database: env('POSTGRES_DB'),
+    synchronize: true,
+    entities: [__dirname + '/**/*.entity{.ts,.js}']
+}
