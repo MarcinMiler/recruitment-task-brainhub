66c631fad2d6613a8ce358b3ff0f8bd7c068e06d
Marcin Miler <marcin4951@gmail.com>
2019-11-05 19:59:36 +0100

    [#12] event module tests frontend

--- frontend/src/modules/event/event.epic.ts
+++ frontend/src/modules/event/event.epic.ts
@@ -1,9 +1,10 @@
 import { Epic } from 'config/rootEpic'
 import { isActionOf } from 'typesafe-actions'
 import { combineEpics } from 'redux-observable'
-import { switchMap, map, catchError, filter, pluck } from 'rxjs/operators'
-import { of } from 'rxjs'
+import { switchMap, map, filter, pluck } from 'rxjs/operators'
+import * as E from 'fp-ts/lib/Either'
 
+import { AppActions } from 'config/rootAction'
 import * as actions from './event.actions'
 import { EventService } from './event.service'
 
@@ -12,12 +13,21 @@ export const eventEpicFactory = (eventService: EventService) => {
         action$.pipe(
             filter(isActionOf(actions.newEventAsync.request)),
             pluck('payload'),
-            switchMap(data =>
-                eventService.newEvent(data).pipe(
-                    map(actions.newEventAsync.success),
-                    catchError(err => of(actions.newEventAsync.failure()))
+            switchMap(data => eventService.newEvent(data)),
+            map(
+                E.fold<string, void, AppActions>(
+                    actions.newEventAsync.failure,
+                    actions.newEventAsync.success
                 )
             )
+            // switchMap(data =>
+            //     eventService.newEvent(data).pipe(
+            //         map(() => actions.newEventAsync.success()),
+            //         catchError(err =>
+            //             of(actions.newEventAsync.failure(err.message))
+            //         )
+            //     )
+            // )
         )
 
     return combineEpics(newEventEpic)
---
+++ frontend/src/modules/event/tests/mocks.ts
@@ -0,0 +1,8 @@
+import { Event } from '../event.model'
+
+export const mockEvent: Event = {
+    firstName: 'Marcin',
+    lastName: 'Miler',
+    email: 'm@m.com',
+    date: '11-11-1111'
+}
